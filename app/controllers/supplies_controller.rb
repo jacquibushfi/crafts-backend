9a62dc81a57e7435bb50117b14982b8986fbd436
jacquibushfi <info@stainedglassofmckinney.com>
2020-03-09 08:22:58 -0500

    creating set_craft in supplies controller and cleaning up controllers

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,51 +1,51 @@
 class SuppliesController < ApplicationController
-  before_action :set_supply, only: [:show, :update, :destroy]
+  before_action :set_craft
 
   # GET /supplies
   def index
-    @supplies = Supply.all
-
-    render json: @supplies
+    supplies = @craft.supplies
+    render json: supplies
   end
 
   # GET /supplies/1
   def show
-    render json: @supply
+    supply = Supply.find_by(id: params[:craft_id])
+    render json: supply
   end
 
   # POST /supplies
   def create
-    @supply = Supply.new(supply_params)
+    supply = @craft.supplies.new(supply_params)
 
-    if @supply.save
-      render json: @supply, status: :created, location: @supply
+    if supply.save
+      render json: supply
     else
-      render json: @supply.errors, status: :unprocessable_entity
+      render json: supply.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /supplies/1
   def update
-    if @supply.update(supply_params)
-      render json: @supply
+    if supply.update(supply_params)
+      render json: supply
     else
-      render json: @supply.errors, status: :unprocessable_entity
+      render json: supply.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /supplies/1
   def destroy
-    @supply.destroy
+    supply.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_supply
-      @supply = Supply.find(params[:id])
+    def set_craft
+      @craft = Craft.find(params[:craft_id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def supply_params
-      params.require(:supply).permit(:craft_id, :description, :vendor, :price)
+      params.require(:supply).permit(:craft_id, :description, :vendor, :price, :id)
     end
 end
